0ed9ee16fccd02bb777baec11c1cd103eff31689
febrianrahmana <febrianrahmana@gmail.com>
2023-04-29 14:58:40 +0700

    Add some more utils and refactor models

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 import typing
 
-FILENAME = typing.Literal["bahan_bangunan.csv", "candi.csv", "user.csv"]
 ROLE_TYPE = typing.Literal["roro_jonggrang", "bandung_bondowoso", "jin_pembangun", "jin_pengumpul"]
-NMAX = 102
 
 class User:
     def __init__(self, arr: list[str, str, ROLE_TYPE]) -> None:
@@ -17,8 +15,17 @@ class Candi:
         self.pasir = arr[2]
         self.batu = arr[3]
         self.air = arr[4]
-        
+
+class Bahan:
+    def __init__(self, arr: list[str,str,int]):
+        self.jenis = arr[0]
+        self.deskripsi = arr[1]
+        self.jumlah = arr[2]
 class Array:
     def __init__(self, arr : list[list[typing.Union[User, Candi]], int]) -> None:
         self.arr = arr[0]
         self.neff = arr[1]
+
+VALID_TYPE = typing.Union[int, str, User, Candi, list]
+FILENAME = typing.Literal["bahan_bangunan.csv", "candi.csv", "user.csv"]
+NMAX = 102
